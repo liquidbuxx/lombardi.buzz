27bc43c1b510462b6f3043b5bcd66bdf015d0a77
buxx <112599956+liquidbuxx@users.noreply.github.com>
2026-02-12 14:30:17 -0500

    fix post message

--- submitIntercept.js
+++ submitIntercept.js
@@ -9,7 +9,8 @@ window.addEventListener("load", function() {
       body: data,
     })
     .then(() => {
-      setTimeout(function(){alert("success! reloading");},3000)
+      alert("success! reloading");
+      setTimeout(function(){},3000);
       window.location.reload();
     })
   });
